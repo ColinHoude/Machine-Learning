526be1fc8efc6ae6eba2ef2cefc91ef4d0e09705
Colin <choude4@gmail.com>
2023-10-10 17:01:52 -0400

    Started programming assignment 3

---
+++ pythonProject/main.py
@@ -0,0 +1,11 @@
+import torch
+
+# test to see if PyTorch is installed properly
+x = torch.rand(5, 3)
+print(x)
+
+
+if __name__ == '__main__':
+    print("Colin Houde - Programming Assignment 2")
+
+
