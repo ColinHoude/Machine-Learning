72cea4f81d0b125247fec1841549949f12fa8b93
Colin <choude4@gmail.com>
2023-10-11 18:04:10 -0400

    Added the CSV for test data and train data

--- HW3/main.py
+++ HW3/main.py
@@ -1,4 +1,13 @@
+import sklearn
+import numpy as np
+import pandas as pd
 
+# loading in the data
+train_data = pd.read_csv("train.csv")
+
+print(train_data.head())
 
 if __name__ == '__main__':
     print("Colin Houde - HW3 - Machine Learning")
+
+    # I added the data cvs to this folder in order to access them
