3bbeafad1ae304cfc0a64d42a7ffbdd88bc72003
Colin <choude4@gmail.com>
2023-10-17 19:08:51 -0400

    More edits on HW3
    I appear to be close to finishing but who knows with these assignments

--- HW3/main.py
+++ HW3/main.py
@@ -1,6 +1,10 @@
 import sklearn
+from sklearn.feature_selection import SelectKBest, chi2
 import numpy as np
 import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+from sklearn import tree
 
 # loading in the data
 # I added the data cvs to this folder in order to access them
@@ -13,6 +17,9 @@ train_data = train_data.dropna(subset=['Embarked'])
 
 # dropping 'Ticket'
 train_data = train_data.drop(columns=['Ticket'])
+
+# dropping 'Name'
+train_data = train_data.drop(columns=['Name'])
 
 
 # Changing the Sex category to numeric
@@ -27,10 +34,37 @@ train_data['Embarked'] = train_data['Embarked'].map(embark_mapping)
 # Change the 'Cabin' to just A,B,C,D... instead of A23, B16, C87
 train_data["Cabin"] = train_data["Cabin"].astype(str).str[:1]
 
-# Change the Cabin category to numeric
-cabin_mapping = {'n': -1, 'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7}
+# Change the Cabin category to numeric,
+# Setting them to decreasing order as cabin A should have a higher influence than cabin G
+cabin_mapping = {'n': 0, 'A': 7, 'B': 6, 'C': 5, 'D': 4, 'E': 3, 'F': 2, 'G': 1}
 train_data['Cabin'] = train_data['Cabin'].map(cabin_mapping)
-print(train_data['Cabin'])
+
+
+# Below is the feature selection section #
+# tempTrainData = train_data.dropna()
+# featSelectX = tempTrainData.drop(columns=['Survived', 'PassengerId'])
+# featSelectY = tempTrainData['Survived']
+
+
+# Select top x features based on mutual info regression
+# X_clf_new = SelectKBest(score_func=chi2, k=1).fit_transform(featSelectX, featSelectY)
+# print(X_clf_new[:10])
+# print(featSelectX.head)
+
+trainDataFeatures = ['Fare', 'Cabin', 'Sex', 'Age', 'Pclass']
+
+finalTrainDataX = train_data[trainDataFeatures]
+finalTrainDataY = train_data['Survived']
+
+
+clf = DecisionTreeClassifier()
+
+# train the classifier
+clf = clf.fit(finalTrainDataX, finalTrainDataY)
+
+y_pred = clf.predict(finalTrainDataX)
+
+print(("Accuracy:", metrics.accuracy_score(finalTrainDataY, y_pred)))
 
 if __name__ == '__main__':
     print("Colin Houde - HW3 - Machine Learning")
